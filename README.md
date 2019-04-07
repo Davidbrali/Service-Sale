7017cf20f475988bef1214e53020a6f25d46ecd9
Gladson <gladsonsouza@live.com>
2019-04-07 12:58:37 -0400

    Firebase com RealTimeDatabase implementado

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Service-Sale
-# Service-Sale
--- js/firebase_user.js
+++ js/firebase_user.js
@@ -1,6 +1,7 @@
 /**
  * Funções para autenticação do usuário no firebase.
  */
+
 var config = {
   apiKey: "AIzaSyAt40bOwxWAS5SiZNnIqtJASkXdDZoLrjs",
   authDomain: "service-sale-projeto.firebaseapp.com",
@@ -13,18 +14,16 @@ var config = {
 firebase.initializeApp(config);
 
 function login() {
-  var email = document.getElementById("input_email").value;
-  var password = document.getElementById("input_password").value;
+  var email = document.getElementById("input_email").value
+  var password = document.getElementById("input_password").value
 
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then(function () {
       alert('Bem-vindo: ' + email)
     })
     .catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
+      console.log(error.code)
+      console.log(error.message)
 
       alert('Não foi possível realizar login!')
     });
@@ -32,20 +31,38 @@ function login() {
 
 function register() {
 
-  var email = document.getElementById("input_email").value;
-  var password = document.getElementById("input_password").value;
+  var email = document.getElementById("input_email").value
+  var password = document.getElementById("input_password").value
+  var username = document.getElementById("input_username").value
+
+  var user = {
+    email: email,
+    name: "",
+    sex: "",
+    cpf: "",
+    age: 0
+  }
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(function () {
-      alert('Novo usuário: ' + email);
+
+      // alert('Novo usuário: ' + email)
+
+      firebase.database().ref('usuarios/' + username).set(user)
+        .then(() => {
+          // alert('Novo usuário: ' + email)
+        })
+        .catch(function (error) {
+          console.error(error.code)
+          console.error(error.message)
+        })
 
     })
     .catch(function (error) {
-      // Handle Errors here.
-      console.error(error.code);
-      console.error(error.message);
+      console.error(error.code)
+      // console.error(error.message)
 
       alert("Não foi possível criar o usuário!");
-      // ...
     });
+
 }
